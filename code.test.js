4a604bb091f5bf9a7ecf7939a5f51d2ad63cfaa4
Andon Mefford <andonmefford@outlook.com>
2023-12-13 14:33:50 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,8 +3,9 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js') + '');
 
-const countOccurrencesProperty = jsc.forall(jsc.array(jsc.number), jsc.number, function (array, key) {
-  return jsc.assertPromise(countOccurrencesAsync(array, key));
+const countOccurrencesProperty = jsc.forall(jsc.array(jsc.number), jsc.number, async function (array, key) {
+  const result = await countOccurrencesAsync(array, key);
+  const expectedCount = array.reduce((acc, element) => (element === key ? acc + 1 : acc), 0);
 });
 
 jsc.assert(countOccurrencesProperty, { tests: 1000 });
