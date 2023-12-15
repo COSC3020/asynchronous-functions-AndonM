e5c0624f3388ff9ef557887bbef2a14f2b294113
Andon Mefford <andonmefford@outlook.com>
2023-12-14 19:16:57 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,10 @@
-/*  
-NOT ENTIRELY SURE IF THIS IS ACTUALLY ASYNC? THE LIBRARY HAS AN ASYNCHIFY FUNCTION
-BUT I FELT I WOULD BE MISSING THE POINT OF THE EXERCISE IN USING IT (I.E. JUST PLUGGING
-IN THE KEY FINDER FUNCTION TO THE ASYNCHIFY FUNCTION) 
-*/
-
 function countOccurrencesAsync(array, key) {
-    if (array.length < 1) {
-        return [], 0;
-    }
-    else {
-        return new Promise((resolve) => {
-          const count = array.reduce((acc, element) => (element === key ? acc + 1 : acc), 0);
-          resolve(count);
-        });
-    }
+  if (array.length < 1) {
+    return Promise.resolve(0);
+  } else {
+    return new Promise((resolve) => {
+      const count = array.reduce((acc, element) => (element === key ? acc + 1 : acc), 0);
+      resolve(count);
+    });
+  }
 }
